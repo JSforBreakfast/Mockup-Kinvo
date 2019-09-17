93a450415b5759a34ea4c17181ff299b73481604
Vitor Emanuel <veslima3@gmail.com>
2019-09-17 06:43:31 -0300

    Add files via upload

---
+++ kinvo/README.md
@@ -0,0 +1,3 @@
+Abra no seu navegador.
+
+Faça o download da pasta kinvo e abra o documento index.html através de um navegador.
---
+++ kinvo/js/actions.js
@@ -0,0 +1,41 @@
+//tab animations
+let tabButtons=document.querySelectorAll(".tabContainer .buttonContainer button");
+let tabPanels=document.querySelectorAll(".tabContainer  .tabPanel");
+
+function showPanel(panelIndex,colorCode) {
+    tabButtons.forEach(function(node){
+        node.style.backgroundColor="";
+        node.style.color="";
+    });
+    tabButtons[panelIndex].style.backgroundColor=colorCode;
+    tabButtons[panelIndex].style.color="#da0a7ab";
+    tabPanels.forEach(function(node){
+        node.style.display="none";
+    });
+    tabPanels[panelIndex].style.display="block";
+    tabPanels[panelIndex].style.backgroundColor=colorCode;
+}
+showPanel(0,'#ffffff');
+
+//tables
+function myFunction() {
+
+  let input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
